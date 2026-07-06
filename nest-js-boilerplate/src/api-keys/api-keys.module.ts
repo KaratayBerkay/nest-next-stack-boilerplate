2da62415fe537eb63c79fe006095b18cd0a6560a
Berkay <karatay.berkay@gmail.com>
2026-07-06 18:42:17 +0300

    fix: import AuthModule in ApiKeysModule for SessionAuthGuard DI resolution
    ApiKeysResolver uses @UseGuards(SessionAuthGuard) which requires JwtService
    from AuthModule. Without the import, NestJS throws:
      Nest can't resolve dependencies of the SessionAuthGuard
      (JwtService not available in ApiKeysModule)

--- nest-js-boilerplate/src/api-keys/api-keys.module.ts
+++ nest-js-boilerplate/src/api-keys/api-keys.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { ApiKeysResolver } from './api-keys.resolver';
 import { ApiKeysService } from './api-keys.service';
 import { ApiKeyGuard } from './api-keys.guard';
 
 @Module({
+  imports: [AuthModule],
   providers: [ApiKeysResolver, ApiKeysService, ApiKeyGuard],
   exports: [ApiKeysService, ApiKeyGuard],
 })
