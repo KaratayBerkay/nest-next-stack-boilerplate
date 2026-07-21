4aaf9782432aba4e29153dbfbc8765c2e43baced
Berkay <karatay.berkay@gmail.com>
2026-07-21 19:18:23 +0300

    fix(v1): add horizontal padding to forms/ui sub-layouts

--- next-js-boilerplate/src/views/forms/FormsLayout.tsx
+++ next-js-boilerplate/src/views/forms/FormsLayout.tsx
@@ -53,7 +53,7 @@ export default function FormsLayout({ children }: FormsLayoutProps) {
         </div>
         <ThemeToggle />
       </div>
-      <div className="flex flex-col gap-6 pb-6">{children}</div>
+      <div className="flex flex-col gap-6 px-4 pb-6 @sm:px-5">{children}</div>
     </div>
   );
 }
