ceb370853cf7745cc9887c22ba1edde532a24006
Berkay <karatay.berkay@gmail.com>
2026-08-04 19:34:43 +0300

    fix: import WireCryptoModule in RealtimeModule to resolve RealtimeGateway dependency

--- nest-js-boilerplate/src/realtime/realtime.module.ts
+++ nest-js-boilerplate/src/realtime/realtime.module.ts
@@ -1,11 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { CryptoModule } from '../common/crypto/crypto.module';
+import { WireCryptoModule } from '../wire-crypto/wire-crypto.module';
 import { RealtimeGateway } from './realtime.gateway';
 import { RealtimePresenceService } from './realtime-presence.service';
 
 @Module({
-  imports: [forwardRef(() => AuthModule), CryptoModule],
+  imports: [forwardRef(() => AuthModule), CryptoModule, WireCryptoModule],
   providers: [RealtimeGateway, RealtimePresenceService],
   exports: [RealtimeGateway],
 })
